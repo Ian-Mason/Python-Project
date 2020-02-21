90fea75bea5be4fa7807a267a6942580429ab4d9
Ian Chisolm <iandchisolm@gmail.com>
2020-02-20 18:46:14 -0600

    Base Code
    Downloads URL to make into log file

---
+++ Pythondownloadcodev2.py
@@ -0,0 +1,18 @@
+"""
+Created on Thu Feb 20 13:02:36 2020
+
+@author: iandc
+"""
+
+import requests
+
+
+print('Beginning file download...')
+
+url = 'https://s3.amazonaws.com/tcmg476/http_access_log'
+myfile = requests.get(url)
+file_location = 'F:/School/TCMG - 412/Python Project/Data.txt'
+
+open(file_location, 'wb').write(myfile.content)
+
+print('File Downloaded')
